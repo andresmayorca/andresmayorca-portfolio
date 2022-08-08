62e91395969c27cf838be743ccc3a07e3bb356d0
ethcode1 <mayorcaalejandro21@gmail.com>
2022-08-08 00:21:13 -0400

    change fonts

--- src/themes/default.js
+++ src/themes/default.js
@@ -1,8 +1,8 @@
 export default {
   // Temp fonts
   fonts: {
-    title: "hack",
-    main: "hack"
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
   },
   // Colors for layout
   colors: {
